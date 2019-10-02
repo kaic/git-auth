3af5b1367a77f90dac13d6d57c351269b0553d03
Kaic Bento <kaicbento@outlook.com>
2019-10-01 22:51:07 -0300

    save storage info in fs

--- bin/git-auth.js
+++ bin/git-auth.js
@@ -10,4 +10,15 @@ program
   .description('run setup for git auth')
   .action(commands.init)
 
+program
+  .command('auth')
+  .description('git-auth authentication')
+  .option('-l, --list', 'List all authentications')
+  .action(commands.init)
+
+program
+  .command('clone')
+  .description('clone with auth enabled')
+  .action(commands.init)
+
 program.parse(process.argv)
